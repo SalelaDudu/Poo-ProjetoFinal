3ec93b9e1bb81b919862dda004fa9eb5e40c0027
Eduardo <coronelcheetos@gmail.com>
2024-12-11 10:01:30 -0300

    inicio de implementação do Util

--- sisbli/infraestrutura/Util.java
+++ sisbli/infraestrutura/Util.java
@@ -1,5 +1,26 @@
 package infraestrutura;
+import java.util.Date;
 
 public class Util {
-    
+    private Util(){}
+
+    public static String obterDataAtual(){
+        Date d = new Date();
+        int dia = d.getDay();
+        int mes = d.getMonth();
+        int ano = d.getYear();
+        String data = "";
+        data += Integer.toString(dia);
+        return 
+    }
+
+    public static String somarDiasData(String stringData, int dias){
+
+        return "";
+    }
+
+    private static Date converterStringData() {
+        Date d = new Date();        
+        return d;
+    }
 }
