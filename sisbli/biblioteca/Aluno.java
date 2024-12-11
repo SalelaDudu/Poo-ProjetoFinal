ec630af930258b81b4a47caf6416d2c1d466e80a
André <andreghiott@gmail.com>
2024-12-11 18:12:53 -0300

    AlunoBibliotecarioFuncionarioProfessorSetor

--- sisbli/biblioteca/Aluno.java
+++ sisbli/biblioteca/Aluno.java
@@ -1,5 +1,28 @@
 package biblioteca;
-
+import java.util.List;
+import java.util.ArrayList;
 public class Aluno {
+ private int matricula;
+ private List<Reserva> reservas = new ArrayList<>();
+ private List<LivroReservado> consumidores = new ArrayList<>();
+ private List<Funcionalidade> funcionalidades = new ArrayList<>();
+//esperar Usuário ficar pronto
+ public Aluno(String nome, String login, String senha, int matricula ){
+     /*
+    super.nome = nome;
+    super.login = login;
+    super.senha = senha;
+    */
+    this.matricula = matricula;
+ }
+
+ public List<LivroReservado> getConsumidores(){
+    return consumidores;
+ }   
+
+ public void cadastrarReserva(List<String> titulosLivros, String data){
+   //esperar implementar a reserva.
+ }
     
+ 
 }
--- sisbli/biblioteca/Bibliotecario.java
+++ sisbli/biblioteca/Bibliotecario.java
@@ -1,5 +1,20 @@
 package biblioteca;
-
+import java.util.List;
+import java.util.Arraylist;
 public class Bibliotecario {
-    
+    private List<Emprestimo> = new ArrayList<>;
+
+    public Bibliotecario(String nome, String senha, Setor lotacao, String cpf ){
+     super.nome = nome;
+     super.login = login;
+     super.senha = senha;
+     super.lotacao = lotacao;
+     super.cpf = cpf;  
+    }
+//esperar Reserva ficar pronto
+/*
+public void cadastrarEmprestimo(){
+
+}
+*/
 }
