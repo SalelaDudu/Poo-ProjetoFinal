3724057e5cbe76c79f6bd7c28332eef5b11d80cf
Eduardo Santos De Assis <20242STADS0012@cefetes.br>
2024-12-17 15:53:05 -0300

    inciio do sonho

--- sisbli/Principal.java
+++ sisbli/Principal.java
@@ -1,16 +1,14 @@
 import javax.swing.JOptionPane;
 import java.util.List;
 import java.util.ArrayList;
-import acesso.Funcionalidade;
+
 import biblioteca.*;
 import divisao.Setor;
 import infraestrutura.Util;
 import acesso.*;
 @SuppressWarnings("unused")
-public class Principal {
-    // ATENCAO  REMOVER TODOS OS COMENTARIOS ANTES DE ENVIAR PARA O CAYO
-    public static void main(String[] args){
-        // String que recebe o título da janeal do programa
+public class Principal {    
+    public static void main(String[] args){        
         String titulo_programa = "Sistema Bibliotecário | v0.1";
         List<Funcionalidade> funcionalidades = new ArrayList<Funcionalidade>();
 
@@ -22,13 +20,25 @@ public class Principal {
         funcionalidades.add(CAD_RESE);
         funcionalidades.add(REM_CONS);
 
-        Professor professor = new Professor("João Souza","joao.souza@professor.com","123456",new Setor("Docente"), "123.456.768-91", funcionalidades);
-        Aluno aluno = new Aluno("José Pereira","jose.pereira@aluno.com","123456",202420012,funcionalidades);
-        Bibliotecario bibliotecario = new Bibliotecario("Marcelo Barbosa","marcelo.barbosa@biblioteca.com","123456",new Setor("Biblioteca"),"123.456.798-55", funcionalidades);
-
         // Recebe o login do usuario
-        // String usuario_login = JOptionPane.showInputDialog(null,"Informe seu login:",titulo_programa,JOptionPane.PLAIN_MESSAGE);
-        // String usuario_senha = JOptionPane.showInputDialog(null,"Informe sua senha:",titulo_programa,JOptionPane.PLAIN_MESSAGE);
+        try{
+
+            String usuario_login = JOptionPane.showInputDialog(null,"Informe seu login:",titulo_programa,JOptionPane.PLAIN_MESSAGE);
+            String usuario_senha = JOptionPane.showInputDialog(null,"Informe sua senha:",titulo_programa,JOptionPane.PLAIN_MESSAGE);
+            
+            if(Usuario.obter(usuario_login, usuario_senha) != null ){
+                Object[] options = { "Cadastrar consumidor", "Remover Consumidor", "Cadastrar Reserva", "Sair" };
+                JOptionPane.showOptionDialog(null, "Selecione uma opção:", titulo_programa,
+                JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
+            null, options, options[0]);
+            }else{
+                JOptionPane.showMessageDialog(null, "Erro ao autenticar usuário.\nEncerrando operação...", titulo_programa, JOptionPane.ERROR_MESSAGE);
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+
 
         // Encerra o programa
         System.exit(0);
--- sisbli/acesso/Usuario.java
+++ sisbli/acesso/Usuario.java
@@ -2,6 +2,7 @@ package acesso;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+import biblioteca.Aluno;
 import biblioteca.Bibliotecario;
 import biblioteca.Funcionario;
 import biblioteca.Livro;
@@ -14,8 +15,15 @@ public class Usuario {
     private String nome;
     private String login;
     private String senha;
-    private List<Funcionalidade> funcionalidades;
+    private List<Funcionalidade> funcionalidades = new ArrayList<>();
     private static List<Usuario> usuarios;
+
+    static{
+        usuarios = new ArrayList<Usuario>();
+        usuarios.add(new Professor("João Souza","joao.souza@professor.com","123456",new Setor("Docente"), "123.456.768-91", null));
+        usuarios.add(new Aluno("José Pereira","jose.pereira@aluno.com","123456",202420012,null));
+        usuarios.add(new Bibliotecario("Marcelo Barbosa","marcelo.barbosa@biblioteca.com","123456",new Setor("Biblioteca"),"123.456.798-55", null));
+    }
 
     public Usuario(String nome, String login, String senha, List<Funcionalidade> funcionalidades){
         this.nome = nome;
@@ -56,7 +64,8 @@ public class Usuario {
     public static Usuario obter(String login, String senha){
         try{
             for (Usuario usuario : usuarios) {
-                if(usuario.login == login && usuario.senha == senha){
+                
+                if((usuario.login.equals(login)) && (usuario.senha.equals(senha))){
                     return usuario;
                 }
             }
